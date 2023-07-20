9edd4e2e9346af5ca5fe36c0128642214db17273
rater31 <85116780+rater31@users.noreply.github.com>
2023-07-20 18:53:51 -0300

    agregue el logo el nav y el h1 con un texti

---
+++ hyg/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+const Header = () => {
+  return (
+    <div className="container-fluid fondoheader">
+        <div className="row text-center">
+
+            <div className="col-12 ">
+                <img src={logo} alt="" srcset="" />
+            </div>
+            <div className="col">
+                <h1>H&G</h1>
+                <p>Developers</p>
+            </div>
+            <div className="col-12 d-flex justify-content-center">
+            <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid">
+          {/* <a className="navbar-brand" href="#">Navbar</a> */}
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link text-light" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">Pricing</a>
+              </li>
+              <li className="nav-item ">
+                <a className="nav-link text-light" href="#">Dropdown link</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
