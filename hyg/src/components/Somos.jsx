ab96a7d5029e5042918807540539da2b8a59cde5
rater31 <fedehidalgo89@gmail.com>
2023-07-24 17:09:58 -0300

    arreglado

--- hyg/src/components/Somos.jsx
+++ hyg/src/components/Somos.jsx
@@ -5,12 +5,12 @@ import Servicios from './Servicios'
 const Somos = () => {
   return (
     <div className='row justify-content-around text-light '>
-        <div className="col-6 col-xxl-2 justify-content-center mt-5 ">
+        <div className="col-6 col-xxl-2  mt-5 ">
             <img className='image' src={pcOscura} alt="" srcset="" />
         </div>
         <div className="col-6  col-xxl-4 text-center mt-5 " >
             <h3>Quienes Somos</h3>
-            <div className="row justify-content-around">
+            <div className="row justify-content-center">
                 <div className="line w-50"></div>
             </div>
             <p className='mt-5'>Somos H&G, una sociedad dedicada a la programación especializada en la parte de fullstack, nos destacamos por ofrecer la creación de páginas web de alta calidad y soluciones innovadoras para nuestros clientes.</p>
