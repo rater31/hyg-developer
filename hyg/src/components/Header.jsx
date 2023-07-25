af7c12b163f859483b75d1d3fd21de2babe8b4ed
Juan Manuel Gonzalez <juanmagonz30@gmail.com>
2023-07-25 03:18:57 -0300

    juanma
    nose que hice pero funciona raro pero funciona xd jsdasjkdhas

--- hyg/src/components/Header.jsx
+++ hyg/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import '../css/header.css'
-import { Oulet, Link } from 'react-router-dom'
-import Ecomerce from './pages/Ecomerce'
-import FormasdeTrabajo from './pages/FormasdeTrabajo'
-import LandinPage from './pages/LandinPage'
-import WebCorporativa from './pages/WebCorporativa'
-import Contacto from './pages/Contacto'
+import { Link } from 'react-router-dom'
+import Ecomerce from '../pages/Ecomerce'
+import FormasdeTrabajo from '../pages/FormasdeTrabajo'
+import LandinPage from '../pages/LandinPage'
+import WebCorporativa from '../pages/WebCorporativa'
+import Contacto from '../pages/Contacto'
 
 const Header = () => {
   return (
@@ -29,13 +29,13 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link to={Header} className="nav-link select"> Inicio </Link> 
+                <Link to="/" className="nav-link select"> Inicio </Link> 
               </li>
               <li className="nav-item">
                 <Link to={FormasdeTrabajo} className="nav-link button2"> Formas de trabajo </Link>
               </li>
               <li className="nav-item">
-                <Link to={LandinPage}className="nav-link button2"> Landing page</Link> 
+                <Link to={LandinPage} className="nav-link button2"> Landing page</Link> 
               </li>
               <li className="nav-item ">
                 <Link to={Ecomerce} className="nav-link button2"> e-commerce </Link>
@@ -49,11 +49,12 @@ const Header = () => {
             </ul>
           </div>
         </div>
+        
       </nav>
             </div>
 
         </div>
-      <Oulet/>
+      
     </div>
   )
 }
