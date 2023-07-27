1c947d6f168bc911d4c1554d383f96590c67ad1e
Juan Manuel Gonzalez <juanmagonz30@gmail.com>
2023-07-27 19:49:17 -0300

    juanma

--- hyg/src/pages/Contacto.jsx
+++ hyg/src/pages/Contacto.jsx
@@ -11,31 +11,31 @@ import '../css/contacto.css'
 const Contacto = () => {
   return (
     <div className='text-light container-fluid fondo-pc'>
-      <div className="row mt-5 fondo-niga">
-        <div className="col-6 text-center">
+      <div className="row mt-3 fondo-niga">
+        <div className="col text-center">
           <h3 className=''>Nos encantaría conocerte mejor</h3>
           <h1 className="">Contáctanos</h1>
           <p className='mt-5 contact'>Estamos encantados de escuchar de usted. Si tiene alguna pregunta o inquietud, no dude en contactarnos. Estamos aquí para ayudarlo a lograr sus objetivos y cumplir sus ideas.</p>
           <div className="row mt-5 text-start">
             <div className="col-6 ">
-              <h3 className='text-start mediante'>Tambien puedes contarcarnos mediante:</h3>
-              <div className="row">
-                <div className="col-12 mt-3">
+              <h3 className='text-center mediante'>Tambien puedes contarcarnos mediante:</h3>
+              <div className="row d-grid justify-content-center">
+                <div className="col mt-3">
                   <span ><img src={logoWp} className='logo-contacto me-2' alt="" srcset="" /> (+54)123 123123</span>
                 </div>
-                <div className="col-12 mt-3">
+                <div className="col mt-3">
                   <span ><img src={logoTel} className='logo-contacto me-2' alt="" srcset="" />(+54)123 123123</span>
                 </div>
-                <div className="col-12 mt-3">
+                <div className="col mt-3">
                 <span ><img src={logoGmail} className='logo-contacto me-2' alt="" srcset="" />correo@correo.com</span>
 
                 </div>
 
               </div>
             </div>
-            <div className="col d-flex justify-content-center">
+            {/* <div className="col d-flex justify-content-center">
               <div className="line3"></div>
-            </div>
+            </div> */}
             <div className="col-5 text-center">
               <h4>Seguinos en nuestras redes</h4>
               <a href="#"><img src={logoInstagram} alt="" srcset="" className='logo-redes' /></a>
@@ -48,8 +48,8 @@ const Contacto = () => {
           </div>
 
         </div>
-
-        <div className="col-6  ">
+        <div className="row mt-5">
+        <div className="col">
           <form className="row g-3 needs-validation  justify-content-center" noValidate>
             <div className="col-7 mt-4">
               {/* <label htmlFor="validationCustom01" className="form-label">Nombre</label> */}
@@ -100,6 +100,8 @@ const Contacto = () => {
           </form>
         </div>
       </div>
+        </div>
+        
 
     </div>
 
