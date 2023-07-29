9f8c2c59f7596598398c687803207b14b80308d1
Juan Manuel Gonzalez <juanmagonz30@gmail.com>
2023-07-29 16:18:10 -0300

    juanma

--- hyg/src/pages/LandinPage.jsx
+++ hyg/src/pages/LandinPage.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import '../css/landing.css'
 const LandinPage = () => {
     return (
-        <div className='hola'>
-            This is the landing page.
+        <div className='text-light container-fluid'>
+            <div className="row mt-3">
+                <div className="col text-center">
+                    <h1 className="">Landin Page</h1>
+                    <h3 className=''>Siempre queremos lo mejor para nuestros clientes por eso ofrecemos el siguiente servicio:</h3>
+                    <p className='text-start'>los servicios son:</p>
+                    <p className='text-start'>Produccion de maqueta</p>
+                    <p className='text-start'>desarrolo de sitio web en Vanila (HTML,CSS,JS)</p>
+
+                    <p></p>
+                </div>
+
+
+            </div>
         </div>
     );
 }
