a55837ea328230994514232b9867ec2e09766f10
Juan Manuel Gonzalez <juanmagonz30@gmail.com>
2023-07-31 18:52:26 -0300

    juanma

--- hyg/src/pages/LandinPage.jsx
+++ hyg/src/pages/LandinPage.jsx
@@ -37,29 +37,29 @@ const LandinPage = () => {
             </div>
             <div className="row justify-content-around mt-pajarito">
             <div className="card" style={{width: '18rem'}}>
-        <img src={estrella} className="card-img-top logo-carta" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-      <div className="card" style={{width: '18rem'}}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-      <div className="card" style={{width: '18rem'}}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
+                    <img src={estrella} className="card-img-top logo-carta" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
             </div>
         </div>
     );
