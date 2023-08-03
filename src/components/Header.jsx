6e49bbe7fc8d41a361ab80c2752575e25655346c
rater31 <85116780+rater31@users.noreply.github.com>
2023-08-03 19:13:27 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,46 +20,11 @@ const Header = () => {
           <img src={logo} alt="" srcset="" />
         </motion.div>
 
-<<<<<<< Updated upstream
             <div className="col">
                 <h1>H&G</h1>
                 <p>Developers</p>
             </div>
             <div className="col d-flex  justify-content-center">
-            <nav className="navbar navbar-expand-md ">
-        <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/"  className="button2">Inicio</Link> 
-              </li>
-              <li className="nav-item">
-                <Link to="/formas-de-trabajo" className="button2 "> Formas de trabajo </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/landing-page"  className="button2"> Landing page</Link> 
-              </li>
-              <li className="nav-item ">
-                <Link to="/e-comerce" id='e-commerce' className="button2"> E-commerce </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/web-corporativa" className="button2"> Web corporativa </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/contacto" className="button2"> Contacto </Link> 
-              </li>
-            </ul>
-          </div>
-=======
-        <div className="col">
-          <h1>H&G</h1>
-          <p>Developers</p>
->>>>>>> Stashed changes
-        </div>
-        <div className="col d-flex  justify-content-center">
           {/* <nav className="navbar navbar-expand-md ">
             <div className="container-fluid">
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
