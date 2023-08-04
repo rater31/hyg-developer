1d6f4745c08d33cab5fefde5d501ea9e2765c807
rater31 <85116780+rater31@users.noreply.github.com>
2023-08-04 18:17:12 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,8 +62,8 @@ const Header = () => {
               </button>
               <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                 <div class="offcanvas-header">
-                  <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
                   <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                  {/* <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">H&G | Developers</h5> */}
                 </div>
                 <div class="offcanvas-body">
                   <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
