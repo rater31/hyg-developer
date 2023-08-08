2359f109dc94b6d450ffbdf3672099543649a42d
rater31 <fedehidalgo89@gmail.com>
2023-08-08 18:20:45 -0300

    Update LandinPage.jsx

--- src/pages/LandinPage.jsx
+++ src/pages/LandinPage.jsx
@@ -19,7 +19,10 @@ const LandinPage = () => {
                     transform: isInView ? "none" : "translateX(-100px)",
                     opacity: isInView ? 1 : 0,
                     transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-                 }}
+                
+                
+                
+                }}
               >
                  {children}
               </span>
