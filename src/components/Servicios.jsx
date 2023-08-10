c62403c0bbb58cc452765eae15c324ea2a60c29d
rater31 <85116780+rater31@users.noreply.github.com>
2023-08-10 12:07:55 -0300

    fede

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -38,7 +38,7 @@ const Servicios = () => {
          <section ref={ref}>
             <span
                style={{
-                  transform: isInView ? "none" : "translateX(100px)",
+                  transform: isInView ? "none" : "translateX(7px)",
                   opacity: isInView ? 1 : 0,
                   transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
                }}
